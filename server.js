8d193e55db39cfb975dc2d378e5bfe21ac63ea39
Yigit Sayan <yigitsayan@gmail.com>
2016-02-25 15:23:11 +0200

    Filtering todos has been adjusted to API

--- server.js
+++ server.js
@@ -13,7 +13,16 @@ app.get('/', function(req, res){
 });
 
 app.get('/todos', function(req, res){
-    res.json(todos);
+    var queryParams = req.query;
+    var filteredTodos = todos;
+
+    if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+        filteredTodos = _.where(filteredTodos, {completed: true})
+    } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+        filteredTodos = _.where(filteredTodos, {completed: false})
+    }
+    
+    res.json(filteredTodos);
 });
 
 app.get('/todos/:id', function(req, res){
