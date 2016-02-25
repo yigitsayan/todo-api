0d27bf3dfb1747179290340b1c2932189efa08b7
Yigit Sayan <yigitsayan@gmail.com>
2016-02-25 15:53:01 +0200

    Search feature for item has been added

--- server.js
+++ server.js
@@ -21,7 +21,13 @@ app.get('/todos', function(req, res){
     } else if(queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos, {completed: false})
     }
-    
+
+    if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+        filteredTodos = _.filter(filteredTodos, function(todo) {
+            return todo.description.indexOf(queryParams.q) > -1;
+        });
+    }
+
     res.json(filteredTodos);
 });
 
