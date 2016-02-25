f9be3999964a8743843b0abd4afbcd8db0c45a80
Yigit Sayan <yigitsayan@gmail.com>
2016-02-25 15:54:36 +0200

    Lowercase for search

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get('/todos', function(req, res){
 
     if(queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
         filteredTodos = _.filter(filteredTodos, function(todo) {
-            return todo.description.indexOf(queryParams.q) > -1;
+            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
         });
     }
 
