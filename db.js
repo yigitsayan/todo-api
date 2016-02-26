d91da3c22fea7f872673bfbb7f76afcf8109d798
Yigit Sayan <yigitsayan@gmail.com>
2016-02-26 15:34:53 +0200

    Sqlite model has been added

---
+++ db.js
@@ -0,0 +1,15 @@
+/**
+ * Created by yigit on 26.2.2016.
+ */
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/data/dev-todo-api.sqlite'
+});
+var db = {};
+
+db.todo = sequelize.import(__dirname + '/models/todo.js');
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
---
+++ models/todo.js
@@ -0,0 +1,19 @@
+/**
+ * Created by yigit on 26.2.2016.
+ */
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('todo', {
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1,250]
+            }
+        },
+        completed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
+};
