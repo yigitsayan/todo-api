d96d83a29a9fe98296bc6773690daac7cbad85b1
Yigit Sayan <yigitsayan@gmail.com>
2016-02-29 14:18:20 +0200

    Add post /todos

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 var _          = require('underscore');
+var db         = require('./db.js');
 var app        = express();
 var PORT       = process.env.PORT || 3000;
 var todos      = [];
@@ -45,7 +46,12 @@ app.get('/todos/:id', function(req, res){
 app.post('/todos', function(req, res){
     var body = _.pick(req.body,'description','completed');
 
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    db.todo.create(body).then(function(todo) {
+       res.json(todo.toJSON());
+    }, function(e) {
+        res.status(400).json(e);
+    });
+    /*if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
         return res.status(404).send();
     }
 
@@ -54,7 +60,7 @@ app.post('/todos', function(req, res){
     todoNextId++;
     todos.push(body);
 
-    res.json(body);
+    res.json(body);*/
 });
 
 app.delete('/todos/:id', function(req, res){
@@ -96,6 +102,8 @@ app.put('/todos/:id', function(req, res){
     res.json(matchedToDo);
 });
 
-app.listen(PORT,function(){
-    console.log('Express listening on port' + PORT + '!');
+db.sequelize.sync().then(function() {
+    app.listen(PORT,function(){
+        console.log('Express listening on port' + PORT + '!');
+    });
 });
