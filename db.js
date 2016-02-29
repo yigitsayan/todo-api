b546b3df748c91754da51f6918dd3f744bbc12ad
Yigit Sayan <yigitsayan@gmail.com>
2016-02-29 14:57:16 +0200

    Add postgres for prod

--- db.js
+++ db.js
@@ -2,10 +2,19 @@
  * Created by yigit on 26.2.2016.
  */
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(undefined, undefined, undefined, {
-    'dialect': 'sqlite',
-    'storage': __dirname + '/data/dev-todo-api.sqlite'
-});
+var env       = process.env.NODE_ENV || 'development';
+var sequelize;
+
+if(env === 'production') {
+    sequelize = new Sequelize(process.env.DATABASE_URL, {
+        'dialect': 'postgres'
+    });
+} else {
+     sequelize = new Sequelize(undefined, undefined, undefined, {
+        'dialect': 'sqlite',
+        'storage': __dirname + '/data/dev-todo-api.sqlite'
+    });
+}
 var db = {};
 
 db.todo = sequelize.import(__dirname + '/models/todo.js');
