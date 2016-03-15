9f19fc7ac98512ac114127469565c32175f80587
Yigit Sayan <yigitsayan@gmail.com>
2016-03-15 12:19:47 +0200

    Hash and salt password

--- models/user.js
+++ models/user.js
@@ -1,6 +1,8 @@
 /**
  * Created by yigit on 29.2.2016.
  */
+var bcrypt = require('bcryptjs');
+var _      = require('underscore');
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('user', {
         email: {
@@ -11,11 +13,25 @@ module.exports = function(sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
             allowNull: false,
             validate: {
                 len: [7,100]
+            },
+            set: function(value) {
+                var salt           = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
@@ -24,6 +40,12 @@ module.exports = function(sequelize, DataTypes) {
                 if(typeof user.email === 'string') {
                     user.email = user.email.toLowerCase();
                 }
+            }
+        },
+        instanceMethods: {
+            toPublicJSON: function() {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
             }
         }
     });
--- server.js
+++ server.js
@@ -114,7 +114,7 @@ app.post('/users', function(req, res) {
     var body = _.pick(req.body, 'email', 'password');
 
     db.user.create(body).then(function(user) {
-        res.json(user.toJSON());
+        res.json(user.toPublicJSON());
     }, function(e) {
         res.status(400).json(e);
     });
